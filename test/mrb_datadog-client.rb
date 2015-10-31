706d578e6ba0a392e265902ca188913f85f2dd27
kappa <kappa@kappabookair.local>
2015-11-01 08:08:20 +0900

    tet

--- test/mrb_datadog-client.rb
+++ test/mrb_datadog-client.rb
@@ -2,16 +2,12 @@
 ## datadog-client Test
 ##
 
-assert("datadog-client#hello") do
-  t = datadog-client.new "hello"
-  assert_equal("hello", t.hello)
-end
-
 assert("datadog-client#bye") do
   t = datadog-client.new "hello"
   assert_equal("hello bye", t.bye)
 end
 
-assert("datadog-client.hi") do
-  assert_equal("hi!!", datadog-client.hi)
+assert("datadog-client#foo") do
+  t = datadog-client.new "hello"
+  assert_equal("hello bye", t.bye)
 end
