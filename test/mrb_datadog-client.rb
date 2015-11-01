9705059e5434d7402dd02cda5a5c2336a34f7c24
inokappa <inokara@gmail.com>
2015-11-01 10:05:30 +0900

    test

--- test/mrb_datadog-client.rb
+++ test/mrb_datadog-client.rb
@@ -3,12 +3,12 @@
 ##
 
 assert("DatadogClient#hello") do
-  t = DatadogClient.new "bye"
+  t = DatadogClient.new "hello"
   assert_equal("Say hello", t.hello)
 end
 
 assert("DatadogClient#hello") do
-  t = DatadogClient.new "hello"
+  t = DatadogClient.new "bye"
   assert_not_equal("Say hello", t.hello)
 end
 
